978babb0991277382ec71cd8e5565244ba556044
adlermore <armorbita@gmail.com>
2020-09-01 19:00:41 +0400

    new function components create

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+function NameTest(props){
+  return (
+  <p>My name is {props.name}</p>
+  );
+}
+
+function SurnameTest(props){
+  return (
+    <h1>My Surname is {props.surname}</h1>
+    );
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> in Love 
         </p>
         <a
           className="App-link"
@@ -16,11 +29,16 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React For Sisian 
         </a>
+        <NameTest name="Ashot" />
+        <SurnameTest surname="Karapetyan" />
+
       </header>
     </div>
   );
 }
+
+
 
 export default App;
